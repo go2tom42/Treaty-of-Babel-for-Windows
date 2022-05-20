3c9a3438918eb7fbbe1d55fcae641963298f458c
tom42 <tom42martin@gmail.com>
2022-05-20 16:03:18 -0400

    Update Install-Script.ps1

--- Install-Script.ps1
+++ Install-Script.ps1
@@ -1,33 +1,26 @@
 Set-MpPreference -DisableRealtimeMonitoring $true
-
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name tom42tools -Force -AllowClobber
 Import-Module -Name tom42tools -Force
 
-If ($env:ChocolateyInstall -eq $null){
-    Install-Choco
-}
+If ($env:ChocolateyInstall -eq $null){Install-Choco}
 Switch-WindowsDefender -Disable
 Set-Location -Path "c:\Users\$env:UserName"
-
 
 Invoke-WebRequest -Uri "https://babel.ifarchive.org/downloads/babel-0.6.zip" -OutFile "C:\Users\$env:UserName\babel-0.6.zip" #Change if not using EVAL version
 Expand-Archive -LiteralPath "C:\Users\$env:UserName\babel-0.6.zip" -DestinationPath "C:\Users\$env:UserName\babel-0.6" #Change if not using EVAL version
 choco feature enable -n allowGlobalConfirmation
 choco install strawberryperl
 
-
-
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 Set-Location -Path "c:\Users\$env:UserName\babel-0.6"
 
 cpanm PAR::Packer #TAKES FOREVER
 refreshenv
-
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 Set-Location -Path "c:\Users\$env:UserName\babel-0.6\extras"
-
 
 pp -o ..\babel-cache.exe babel-cache.pl
 pp -o ..\babel-infocom.exe babel-infocom.pl
@@ -35,20 +28,17 @@ pp -o ..\babel-marry.exe babel-marry.pl
 pp -o ..\babel-wed.exe babel-wed.pl
 
 Set-Location -Path "c:\Users\$env:UserName\babel-0.6"
-
 choco install make mingw upx
 refreshenv
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
 (Get-Content treaty.h) -Replace 'Version 0.5', 'Version 0.6' | Set-Content treaty.h
 (Get-Content treaty.h) -Replace '#define TREATY_COMPLIANCE "Treaty of Babel revision 10"', '#define TREATY_COMPLIANCE "Treaty of Babel revision 11"' | Set-Content treaty.h
 (Get-Content treaty.h) -Replace '#define TREATY_VERSION "r10"', '#define TREATY_VERSION "r11"' | Set-Content treaty.h
-
 (Get-Content babel.h) -Replace '#define BABEL_VERSION "0.5"', '#define BABEL_VERSION "0.6"' | Set-Content babel.h
-
 Copy-Item babel_handler.h extras/babel_handler.h
 Copy-Item ifiction.h extras/ifiction.h
 Copy-Item treaty.h extras/treaty.h
-
 Copy-Item babel_handler.h babel-get/babel_handler.h
 Copy-Item ifiction.h babel-get/ifiction.h
 Copy-Item treaty.h babel-get/treaty.h
@@ -107,9 +97,7 @@ ar cr zcode.a zcode.o
 
 ar -r babel_functions.a babel_story_functions.o babel_ifiction_functions.o babel_multi_functions.o
 gcc -o babel babel.o babel_functions.a ifiction.a babel.a babel_handler.a md5.a blorb.a misc.a register_ifiction.a register.a executable.a zcode.a glulx.a tads2.a tads3.a hugo.a alan.a adrift.a level9.a agt.a magscrolls.a advsys.a html.a tads.a
-
 Set-Location -Path "c:\Users\$env:UserName\babel-0.6\extras"
-
 (Get-Content hotload.c) -Replace 'int babel_hotload', 'int main' | Set-Content hotload.c
 
 gcc -c babel-list.c
@@ -130,7 +118,6 @@ gcc -o ..\simple-marry simple-marry.o ..\ifiction.a ..\babel_handler.a ..\misc.a
 gcc -o ..\babel-list babel-list.o ..\babel_handler.a ..\misc.a ..\md5.a ..\register.a ..\adrift.a ..\advsys.a ..\alan.a ..\blorb.a ..\executable.a ..\glulx.a ..\html.a ..\hugo.a ..\magscrolls.a ..\tads2.a ..\tads3.a ..\zcode.a ..\agt.a ..\level9.a ..\ifiction.a ..\tads.a ..\register_ifiction.a
 gcc -o ..\hotload hotload.o ..\ifiction.a ..\babel_handler.a ..\misc.a ..\register_ifiction.a ..\md5.a ..\register.a ..\glulx.a ..\html.a ..\hugo.a ..\magscrolls.a ..\tads2.a ..\tads3.a ..\zcode.a ..\agt.a ..\level9.a ..\alan.a ..\adrift.a ..\advsys.a ..\executable.a ..\blorb.a ..\tads.a
 
-
 Set-Location -Path "c:\Users\$env:UserName\babel-0.6\babel-get"   
 
 gcc -c babel-get.c
@@ -148,9 +135,7 @@ ar cr get_url.a get_url.o
 gcc -o ..\babel-get babel-get.o get_dir.a get_ifiction.a get_story.a get_url.a ..\ifiction.a ..\babel_handler.a ..\misc.a ..\register_ifiction.a ..\md5.a ..\register.a ..\glulx.a ..\html.a ..\hugo.a ..\magscrolls.a ..\tads2.a ..\tads3.a ..\zcode.a ..\agt.a ..\level9.a ..\alan.a ..\adrift.a ..\advsys.a ..\executable.a ..\blorb.a ..\tads.a
 
 Set-Location -Path "c:\Users\$env:UserName\babel-0.6"
-
 upx -9 *.exe
 Compress-Archive -Path *.exe -DestinationPath "C:\Users\$env:UserName\Desktop\babel_suite_win32.zip" #Change if not using EVAL version
 Set-MpPreference -DisableRealtimeMonitoring $false
-
 Switch-WindowsDefender -Enable
